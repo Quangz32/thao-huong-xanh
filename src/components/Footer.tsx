8035c269ad93ca59783700c2400593454dd7a747
Quangz <quangzdrive>
2025-07-11 21:23:16 +0700

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,15 +11,21 @@ export default function Footer() {
               <div className="text-white text-xl mb-2">
                 Địa chỉ: Xã Thạch Hoà, Thạch Thất, Hà Nội
               </div>
-              <div className="text-white text-xl mb-2">Thông tin liên hệ: 0392.942.596</div>
+              <div className="text-white text-xl mb-2">
+                Thông tin liên hệ: 0392.942.596
+              </div>
               <div className="flex">
                 <img src="/img/qr-fanpage.png" className="w-24 h-24 mb-2"></img>
                 <span className="ml-4 text-xl text-white pt-16">Fanpage</span>
               </div>
             </div>
-            <div>
-              <div className="mt-4 text-white text-4xl mb-2">NSX: Mịn Màng Việt Nam</div>
-              <div className="text-white text-xl mb-2">Địa chỉ: Quận 9, TP Hồ Chí Minh</div>
+            <div className="my-auto">
+              <div className="mt-4 text-white text-2xl mb-2">
+                NSX: Mịn Màng Việt Nam
+              </div>
+              <div className="text-white text-base mb-2">
+                Địa chỉ: Quận 9, TP Hồ Chí Minh
+              </div>
             </div>
           </div>
           {/* <div className="text-2xl text-white">Footer ne</div> */}
